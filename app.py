bbc5da6e0cd93524d7e14429932796b1cee11722
Kiuliumov <miti.bania@gmail.com>
2024-02-17 14:24:39 +0200

    daily command

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+# todo implement command handling and slash commands
+# todo implement weekly demon command
+# todo implement level searching based on id or level name
+# todo implement account searching system
+# todo implement pack searching system
 import discord
 import gd
-from utils.get_image import get_image
-
+from utils import util_functions
+import random
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -21,15 +26,32 @@ async def on_message(message):
 
     if message.content.startswith('$ping'):
         await message.channel.send('Pong!')
+
     elif message.content.startswith('$daily'):
         daily = await gd_client.get_daily()
-        image = get_image(daily.stars)
-        embed = discord.Embed(color=0x7289da).set_author(name="Current Daily")
-        embed.set_image(url=image)
+        image = util_functions.get_image(daily.stars)
+        embed = discord.Embed(color=util_functions.generate_random_hex_int()).set_author(name="Current Daily")
+        embed.set_thumbnail(url=image)
         embed.add_field(name="Name", value=daily.name)
         embed.add_field(name="Difficulty", value=f"{daily.stars} stars")
         embed.add_field(name="ID", value=f"{daily.id}")
         embed.set_footer(text=f"Creator: {daily.creator.name}")
         await message.channel.send(embed=embed)
 
-client.run()
+    elif message.content.startswith('$song'):
+        try:
+            random_color = ''.join(["#"+''.join([random.choice('ABCDEF0123456789') for i in range(6)])])
+            song_id = message.content.split()[1]
+            song = await gd_client.get_song(song_id)
+            embed = discord.Embed(color=util_functions.generate_random_hex_int()).set_author(name=song.name)
+            embed.set_thumbnail(url='https://i.redd.it/r30pn3cei4r81.png')
+            embed.add_field(name="Song author", value=song.artist.name)
+            embed.add_field(name="Song ID", value=song.id)
+            embed.add_field(name="Song size", value=song.size)
+            embed.add_field(name="Link", value=song.url)
+            embed.set_footer(text=f"By the Cantina®")
+            await message.channel.send(embed=embed)
+        except Exception as e:
+            await message.channel.send(f'Enter a valid song id! {e}')
+
+client.run('ODMwODIxMTE0MjY3MTcyOTA1.GMdeu2.kzekyW6prlzYL4h6C-7XUa_iVAj5szmZRsMK0Q')
