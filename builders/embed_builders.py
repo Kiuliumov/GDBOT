e8d5acfed945018919b6145a2feff61b9889b504
Kiuliumov <miti.bania@gmail.com>
2024-02-17 17:42:24 +0200

    daily command

--- builders/embed_builders.py
+++ builders/embed_builders.py
@@ -1,14 +1,16 @@
 from client import gd_client
 import discord
-from utils import util_functions
+from image_processing import get_image, get_demon_image
+from utils.util_functions import generate_random_hex_int
+
 
 async def make_level_embed(level_id: int):
     level = await gd_client.get_level(level_id)
     if level.stars == 10:
-        image = util_functions.get_demon_image(difficulty=level.difficulty)
+        image = get_demon_image(difficulty=level.difficulty)
     else:
-        image = util_functions.get_image(difficulty=level.difficulty)
-    embed = discord.Embed(color=util_functions.generate_random_hex_int()).set_author(name=level)
+        image = get_image(difficulty=level.difficulty)
+    embed = discord.Embed(color=generate_random_hex_int()).set_author(name=level)
     embed.set_thumbnail(url=image)
     embed.add_field(name='Description:', value=level.description, inline=True)
     embed.add_field(name='Version', value=level.version)
@@ -23,7 +25,7 @@ async def make_level_embed(level_id: int):
 
 async def make_song_embed(song_id: int):
     song = await gd_client.get_song(song_id)
-    embed = discord.Embed(color=util_functions.generate_random_hex_int()).set_author(name=song.name)
+    embed = discord.Embed(color=generate_random_hex_int()).set_author(name=song.name)
     embed.set_thumbnail(url='https://i.redd.it/r30pn3cei4r81.png')
     embed.add_field(name="Song author", value=song.artist.name)
     embed.add_field(name="Song ID", value=song.id)
