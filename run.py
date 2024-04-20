b5231c2433c9fc5fb35ef445c5e849bc5e16230a
Kiuliumov <miti.bania@gmail.com>
2024-04-20 19:34:45 +0300

    app

--- run.py
+++ run.py
@@ -4,12 +4,17 @@ import subprocess
 import customtkinter
 from PIL import Image, ImageTk
 
+
 def change_token_on_click(token: str):
 
     if len(token) < 50:
         raise ValueError("Token must be at least 50 characters long")
 
     constants.TOKEN = token
+
+def change_prefix(prefix: str):
+    constants.PREFIX = prefix
+
 
 def run_bot():
     subprocess.run(['run.bat'], shell=True)
